1009283ba308f8555c974ce003d28cd76a6c3dee
hucatherine <112427945+hucatherine@users.noreply.github.com>
2022-12-06 22:22:10 -0500

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("top");
+
+
+// scroll to top
+function scrollUp() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
